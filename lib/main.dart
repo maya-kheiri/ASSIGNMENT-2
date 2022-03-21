5692ddccda54abd3927fb1e187e3954be242a3b5
Maya Kronz <86155416+maya-kheiri@users.noreply.github.com>
2022-03-21 02:59:17 -1000

    commit.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,52 @@
+import 'package:assignment_2/widget/button_widget.dart';
+import 'package:assignment_2/widget/navigation_drawer_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  static const String title = 'Navigation Drawer';
 
-  // This widget is the root of your application.
+  MyApp({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', theme: ThemeData());
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: title,
+    theme: ThemeData(primarySwatch: Colors.blue),
+    home: const MainPage(),
+  );
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    drawer: const NavigationDrawerWidget(),
+    appBar: AppBar(
+      backgroundColor: const Color(0xFF880E4F),
+      title: const Text(MyApp.title),
+    ),
+    body: Builder(
+      builder: (context) => Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+      ),
+    ),
+  );
 }
