e76f47870aa1a1a700f07bd12cff11a6aff0c2b3
Maya Kronz <86155416+maya-kheiri@users.noreply.github.com>
2022-03-21 04:07:32 -1000

    commit.

--- lib/page/favourites_page.dart
+++ lib/page/favourites_page.dart
@@ -22,6 +22,7 @@ class FavouritesPage extends StatelessWidget {
     ),
     floatingActionButton: FloatingActionButton(
       child: const Icon(Icons.rotate_left),
+      backgroundColor: Colors.pink,
       onPressed: () {
         final isPortrait =
             MediaQuery.of(context).orientation == Orientation.portrait;
@@ -69,14 +70,14 @@ class FavouritesPage extends StatelessWidget {
 
   Widget buildText() => Column(
     children: const [
-      Text(
-        'Maya Kheiri',
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 32,
-          color: Color(0xFF3949AB),
-        ),
-      ),
+      // Text(
+      //   'Maya Kheiri',
+      //   style: TextStyle(
+      //     fontWeight: FontWeight.bold,
+      //     fontSize: 32,
+      //     color: Color(0xFF3949AB),
+      //   ),
+      // ),
       SizedBox(height: 16),
       // Text(
       //   '''The oldest known depiction of hair styling is hair braiding which dates back about 30,000 years. In history, women's hair was often elaborately and carefully dressed in special ways. From the time of the Roman Empire[citation needed] until the Middle Ages, most women grew their hair as long as it would naturally grow. Between the late 15th century and the 16th century, a very high hairline on the forehead was considered attractive. Around the same time period, European men often wore their hair cropped no longer than shoulder-length. In the early 17th century, male hairstyles grew longer, with waves or curls being considered desirable.''',
